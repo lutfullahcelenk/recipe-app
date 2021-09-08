b49745521bfdaa84e8af87170928debb1704b2da
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-08 20:56:05 +0300

    Navbar section is written inside the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from "./components/navbar/Navbar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      Hello
+        <Navbar />
     </div>
   );
 }
