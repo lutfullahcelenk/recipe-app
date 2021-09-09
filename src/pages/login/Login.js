ebfd8460593c736c654f71dad8d71fe98a3d5218
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-09 20:38:10 +0300

    Login page is added and styled

---
+++ src/pages/login/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from './LoginStyle';
+import mealSvg from '../../assets/meal2.svg';
+const Login = ({ setIsAuth }) => {
+  let history = useHistory();
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    setIsAuth(true);
+    history.push('/');
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} alt="meal" />
+        <Header>{'<Clarusway/>'} Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="Username" required />
+          <StyledInput type="password" placeholder="Password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+export default Login;
---
+++ src/pages/login/LoginStyle.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+export const LoginContainer = styled.div`
+  background-image: url('https://picsum.photos/1600/900');
+  background-repeat: no-repeat;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const FormContainer = styled.div`
+  width: 600px;
+  min-width: 600px;
+  height: 600px;
+  background-color: rgba(0, 173, 181, 0.5);
+  border-radius: 50%;
+  border: 2px solid #E1F1DD;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+export const Header = styled.h1`
+  color: white;
+  font-family: 'Girassol', sans-serif;
+  font-size: 3rem;
+`;
+export const StyledInput = styled.input`
+  height: 50px;
+  font-size: 2rem;
+  width: 250px;
+  border-radius: 5px;
+  font-family: 'Girassol', sans-serif;
+  background-color: rgba(0, 0, 0, 0.6);
+  border: none;
+  color: white;
+  margin: 1rem;
+  text-indent: 20px;
+`;
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+export const StyledButton = styled.button`
+  font-size: 2rem;
+  font-family: 'Girassol', sans-serif;
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin: 1rem;
+`;
+export const StyledImg = styled.img`
+  width: 150px;
+  margin: 1rem;
+`;
